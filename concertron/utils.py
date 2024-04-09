40adff656ad6f9fea7766e91f543e371b4705a90
thanndbt <daanvorstermans@gmail.com>
2024-04-09 23:39:21 +0200

    Added image downloader

--- concertron/utils.py
+++ concertron/utils.py
@@ -2,6 +2,9 @@ import pymongo
 from scrapy.utils.project import get_project_settings
 from datetime import datetime, timedelta
 
+from io import BytesIO
+import requests
+
 settings = get_project_settings()
 
 mongo_uri=settings.get('MONGODB_URI')
@@ -21,3 +24,16 @@ def does_event_exist(_id):
     else:
         return "EVENT_DOES_NOT_EXIST"
 
+def download_image(url):
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
+        b = BytesIO(r.content).read()
+        return b
+    except requests.RequestException as e:
+        print(f"Error making request to {url}: {e}")
+        return None
+    except Exception as e:
+        print(f"Something went wrong with {url}, but status was 200: {e}")
+
+    
