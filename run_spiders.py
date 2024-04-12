0af64f63d5815e2ade52ae2f9d34cf7daa151142
thanndbt <daanvorstermans@gmail.com>
2024-04-13 00:31:14 +0200

    Have spiders load dynamically

--- run_spiders.py
+++ run_spiders.py
@@ -5,11 +5,14 @@ from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
 from scrapy.utils.reactor import install_reactor
+import importlib
+import pkgutil
 
 # install_reactor('twisted.internet.asyncioreactor.AsyncioSelectorReactor')
 # asyncioreactor.install()
 
-from concertron.spiders import nl_melkweg, nl_013, nl_paradiso, nl_tivolivredenburg
+# from concertron.spiders import nl_melkweg, nl_013, nl_paradiso, nl_tivolivredenburg
+from concertron.spiders import *
 
 settings = get_project_settings()
 configure_logging(settings)
@@ -17,16 +20,23 @@ runner = CrawlerRunner(settings)
 
 @defer.inlineCallbacks
 def crawl():
-    yield runner.crawl(nl_013.spiderEvents)
-    yield runner.crawl(nl_013.spiderTags)
-    yield runner.crawl(nl_melkweg.spider)
-    yield runner.crawl(nl_paradiso.spiderEvents)
-    yield runner.crawl(nl_paradiso.spiderTags)
-    yield runner.crawl(nl_tivolivredenburg.spiderEvents)
-    yield runner.crawl(nl_tivolivredenburg.spiderTags)
+    # yield runner.crawl(nl_013.spiderEvents)
+    # yield runner.crawl(nl_013.spiderTags)
+    # yield runner.crawl(nl_melkweg.spider)
+    # yield runner.crawl(nl_paradiso.spiderEvents)
+    # yield runner.crawl(nl_paradiso.spiderTags)
+    # yield runner.crawl(nl_tivolivredenburg.spiderEvents)
+    # yield runner.crawl(nl_tivolivredenburg.spiderTags)
+
+    spiders_package = importlib.import_module("concertron.spiders")
+    for importer, modname, ispkg in pkgutil.iter_modules(spiders_package.__path__):
+        module = importlib.import_module(f"concertron.spiders.{modname}")
+        for name in dir(module):
+            obj = getattr(module, name)
+            if isinstance(obj, type) and issubclass(obj, scrapy.Spider) and obj != scrapy.Spider:
+                yield runner.crawl(obj)
+
     reactor.stop()
-# d = runner.join()
-# d.addBoth(lambda _: reactor.stop())
 
 crawl()
 reactor.run()
