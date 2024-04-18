9a06fcc951049d6a0e5f8615bd62c25d047f79ef
thanndbt <daanvorstermans@gmail.com>
2024-04-18 21:40:06 +0200

    Added simple tagger page

--- webapp/app.py
+++ webapp/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, jsonify, session
 from pymongo import MongoClient
 from datetime import datetime
+import secrets
 
 app = Flask(__name__)
 
@@ -9,13 +10,15 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client['concertron_test']  # Change this to your database name
 collection = db['events']  # Change this to your collection name
 
+app.secret_key = secrets.token_hex(16)
+tag_list = db.events.distinct('tags', filter={'$or': [{'event_type': 'Concert'}, {'event_type': 'Club'}, {'event_type': 'Festival'}]})
 
 @app.route('/')
 def index():
     # Fetch data from MongoDB
     data = list(collection.find(filter={
         'event_type': 'Concert',
-        'date': {'$gt': datetime.today()}
+        'date': {'$gt': datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)}
         }, sort = {
             'date': 1
             }))  # Fetch all documents
@@ -27,7 +30,7 @@ def filter_data():
     print(request.form.get('date'), str(type(request.form.get('date'))))
     filter = {
             'event_type': 'Concert',
-            'date': {'$gt': datetime.today()}
+            'date': {'$gt': datetime.now()}
             }
     for key, value in request.form.items():
         if key == 'status':
@@ -37,10 +40,6 @@ def filter_data():
                     filter['status'] = checked[0]
                 else:
                     filter['$or'] = [{'status': status} for status in checked]
-                    # select = []
-                    # for status in checked:
-                        # select.append({'status': status})
-                    # filter.update({'$or': select})
         elif key == 'date':
             if value:
                 filter['date'] = {'$gt': datetime.fromisoformat(value)}
@@ -55,5 +54,55 @@ def serve_image(filename):
     img_dir = '../img/'
     return send_from_directory(img_dir, filename)
 
+@app.route('/tagger', methods=['GET'])
+def tagger():
+    session['list_index'] = 0
+    while db.tags.find_one({'_id': tag_list[session['list_index']]}) is not None:
+        # print(session['list_index'])
+        # print(db.tags.find_one({'_id': tag_list[session['list_index']]}))
+        session['list_index'] += 1
+    tag = tag_list[session['list_index']]
+    meta_tags = db.tags.distinct('meta_tags')
+    genre_tags = db.tags.distinct('genre_tags')
+    special_tags = db.tags.distinct('special_tags')
+    return render_template('tagger.html', tag=tag, metas=meta_tags, genres=genre_tags, specials=special_tags)
+
+@app.route('/tagger/submit', methods=['POST'])
+def submit():
+    form = request.form
+    new_meta_tags = []
+    new_genre_tags = []
+    new_special_tags = []
+
+    if form.get('new_meta'):
+        new_meta_tags.extend(form.get('new_meta').split(', '))
+    if form.get('new_genre'):
+        new_genre_tags.extend(form.get('new_genre').split(', '))
+    if form.get('new_special'):
+        new_special_tags.extend(form.get('new_special').split(', '))
+    if form.get('exist_meta'):
+        new_meta_tags.extend(form.getlist('exist_meta'))
+    if form.get('exist_genre'):
+        new_genre_tags.extend(form.getlist('exist_genre'))
+    if form.get('exist_special'):
+        new_special_tags.extend(form.getlist('exist_special'))
+
+    db.tags.insert_one({
+        '_id': tag_list[session['list_index']],
+        'meta_tags': new_meta_tags,
+        'genre_tags': new_genre_tags,
+        'special_tags': new_special_tags
+        })
+
+
+    meta_tags = db.tags.distinct('meta_tags')
+    genre_tags = db.tags.distinct('genre_tags')
+    special_tags = db.tags.distinct('special_tags')
+    session['list_index'] += 1
+    while db.tags.find_one({'_id': tag_list[session['list_index']]}) is not None:
+        session['list_index'] += 1
+    tag = tag_list[session['list_index']]
+    return jsonify(tag=tag, metas=meta_tags, genres=genre_tags, specials=special_tags)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
